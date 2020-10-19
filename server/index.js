472385dfe5f79cb1412ab3ee85730be91c999a78
Dakota Benger <dakota@bengerlaw.com>
2020-10-19 16:31:19 -0700

    Starting Phase 5

--- server/index.js
+++ server/index.js
@@ -2,17 +2,29 @@ const http = require('http');
 const hostname = '127.0.0.1'; 
 const port = 8081;  
 const { readFile } = require('fs').promises;
+const path = require('path');
+
 
 const server = http.createServer(async (req, res) => {
-    if (req.url.match(/^images/)) {
+    if (req.url.match(/^\/images/)) {
+        try {
         const imageFilePath = './assets' + req.url;
+        const fileExtension = await path.extname(imageFilePath);
         const imageFileContents = await readFile(imageFilePath);
-    }
-    
-    
+        const imageType = 'image/' + fileExtension.substring(1);
+        res.statusCode = 200; 
+        res.setHeader('Content-Type', imageType);
+        res.end(imageFileContents);
+        } catch(e) {
+            res.statusCode = 404
+            res.end()
+            return
+        }
+    } else {
     res.statusCode = 200; 
     res.setHeader('Content-Type', 'text/plain');
     res.end('I have items');
+    }
 }); 
 
 server.listen(port, hostname, () => {
